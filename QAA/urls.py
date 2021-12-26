8a5781773de0bac8bf74cbd0a9a39fdce549533b
oraby8 <oraby888@gmail.com>
2021-12-26 17:43:15 +0200

    'v5'

--- QAA/urls.py
+++ QAA/urls.py
@@ -22,9 +22,6 @@ urlpatterns = [
     path('', include('Qapp.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += (
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        )
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
